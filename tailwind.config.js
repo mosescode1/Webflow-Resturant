e74c4a924ca0802e7b1c5f0b700d613b52a5d71f
mosescode1 <emosescode@gmail.com>
2023-07-25 00:43:09 +0100

    added the inter google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./dist/*.{html,js}'], // Specify the files to be analyzed for purging unused CSS classes
+  purge: ["./dist/*.{html,js}"], // Specify the files to be analyzed for purging unused CSS classes
   theme: {
     extend: {
       colors: {
         customOrange: "#E1B168",
+        btnColor: "#4353FF",
+      },
+      fontFamily: {
+        sanss: ["Inter", "Helvetica", "Arial", "sans-serif"],
       },
     },
   },
